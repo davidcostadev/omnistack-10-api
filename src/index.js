cdc2ce62b6e534a6cd132ae0f2be947da8e02a7b
David Costa <davidcostadev@gmail.com>
2020-01-19 11:33:50 -0300

    Add express base

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('file')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({
+    hello: 'world',
+  });
+});
+
+app.listen(3000, () => console.log('Server started on 3000 port.'));
