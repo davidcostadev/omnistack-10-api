88d20a68791af9999d1ae7e24f8185b387c3c936
David Costa <davidcostadev@gmail.com>
2020-01-19 14:26:01 -0300

    Create a dev Controller

---
+++ src/controllers/DevController.js
@@ -0,0 +1,71 @@
+const axios = require('axios');
+const get = require('lodash.get');
+const Dev = require('../models/Dev');
+
+const create = async (req, res) => {
+  const { githubUserName, techs, latitude, longitude } = req.body;
+
+  try {
+    const response = await axios.get(`https://api.github.com/users/${githubUserName}`);
+    const { name = login, avatar_url: avatarUrl, bio } = response.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const location = {
+      type: 'Point',
+      coordinates: [latitude, longitude],
+    };
+
+    const dev = await Dev.create({
+      githubUserName,
+      name,
+      avatarUrl,
+      bio,
+      techs: techsArray,
+      location,
+    });
+
+    return res.json(dev);
+  } catch (error) {
+    const response = {
+      error: get(error, 'response.data.message', error.message),
+    };
+    console.error(response);
+    return res.status(get(error, 'response.status', 400)).json();
+  }
+};
+
+const getList = async (req, res) => {
+  try {
+    const devs = await Dev.find();
+    res.json(devs);
+  } catch (error) {
+    const response = {
+      error: get(error, 'response.data.message', error.message),
+    };
+    console.error(response);
+
+    return res.status(get(error, 'response.status', 400)).json(response);
+  }
+};
+
+const destroy = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Dev.findOneAndDelete(id);
+
+    res.status(204).send();
+  } catch (error) {
+    const response = {
+      error: get(error, 'response.data.message', error.message),
+    };
+    console.error(response);
+    return res.status(get(error, 'response.status', 400)).json(response);
+  }
+};
+
+module.exports = {
+  create,
+  getList,
+  destroy,
+};
--- src/routers.js
+++ src/routers.js
@@ -1,70 +1,12 @@
 const { Router } = require('express');
-const axios = require('axios');
-const get = require('lodash.get');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
 
 const routers = Router();
 
-routers.get('/devs', async (req, res) => {
-  try {
-    const devs = await Dev.find();
-    res.json(devs);
-  } catch (error) {
-    const response = {
-      error: get(error, 'response.data.message', error.message),
-    };
-    console.error(response);
+routers.get('/devs', DevController.getList);
 
-    return res.status(get(error, 'response.status', 400)).json(response);
-  }
-});
+routers.delete('/devs/:id', DevController.destroy);
 
-routers.delete('/devs/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    await Dev.findOneAndDelete(id);
-
-    res.status(204).send();
-  } catch (error) {
-    const response = {
-      error: get(error, 'response.data.message', error.message),
-    };
-    console.error(response);
-    return res.status(get(error, 'response.status', 400)).json(response);
-  }
-});
-
-routers.post('/devs', async (req, res) => {
-  const { githubUserName, techs, latitude, longitude } = req.body;
-
-  try {
-    const response = await axios.get(`https://api.github.com/users/${githubUserName}`);
-    const { name = login, avatar_url: avatarUrl, bio } = response.data;
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-      type: 'Point',
-      coordinates: [latitude, longitude],
-    };
-
-    const dev = await Dev.create({
-      githubUserName,
-      name,
-      avatarUrl,
-      bio,
-      techs: techsArray,
-      location,
-    });
-
-    return res.json(dev);
-  } catch (error) {
-    const response = {
-      error: get(error, 'response.data.message', error.message),
-    };
-    console.error(response);
-    return res.status(get(error, 'response.status', 400)).json();
-  }
-});
+routers.post('/devs', DevController.create);
 
 module.exports = routers;
